d45a670fdbcd999f7c9ae9a6c6d406959ff92f10
lmt019 <934808050@qq.com>
2022-01-21 12:45:00 +0800

    chore: init

---
+++ src/main/java/com/agile/train/TrainApplication.java
@@ -0,0 +1,15 @@
+package com.agile.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class TrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/agile/train/config/WebMvcConfiguration.java
@@ -0,0 +1,17 @@
+package com.agile.train.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //NOTE swagger的配置
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
