6b817166e21267d8ae3d675a6d7c22cb9a71869b
lmt019 <934808050@qq.com>
2022-01-23 12:38:34 +0800

    chore: try action

--- src/main/java/com/agile/train/controller/HelloController.java
+++ src/main/java/com/agile/train/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.agile.train.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,5 +14,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello world!";
+    }
+
+    @GetMapping("/h")
+    public String h(@RequestParam String inp){
+        return "hello "+inp+"!";
     }
 }
