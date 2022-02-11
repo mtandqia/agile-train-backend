a6dea396e0a37e19468816b4166277d7b7e9b1a8
WangQY17 <wqy17nju@163.com>
2022-02-11 22:19:57 +0800

    fix: swagger

--- src/main/java/com/agile/train/controller/AccountController.java
+++ src/main/java/com/agile/train/controller/AccountController.java
@@ -34,7 +34,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/train/admin")
 @Slf4j
-@EnableWebMvc
+//@EnableWebMvc
 public class AccountController {
     @Autowired
     UserService userService;
