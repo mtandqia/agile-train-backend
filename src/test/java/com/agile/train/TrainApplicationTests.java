6e25827412ecc75111b9c9c6fab8523169b741ac
WangQY17 <wqy17nju@163.com>
2022-02-12 22:58:48 +0800

    fix: reset

--- src/test/java/com/agile/train/TrainApplicationTests.java
+++ src/test/java/com/agile/train/TrainApplicationTests.java
@@ -1,7 +1,7 @@
 package com.agile.train;
 
-//import org.junit.Test;
-import org.testng.annotations.Test;
+import org.junit.Test;
+//import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- src/test/java/com/agile/train/service/UserServiceTest.java
+++ src/test/java/com/agile/train/service/UserServiceTest.java
@@ -4,8 +4,8 @@ import com.agile.train.dto.UserDTO;
 import com.agile.train.dto.UserModifyDTO;
 import com.agile.train.entity.User;
 
-//import org.junit.Test;
-import org.testng.annotations.Test;
+import org.junit.Test;
+//import org.testng.annotations.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
