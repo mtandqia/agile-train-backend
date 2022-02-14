77ae3d73e09e1adcca2b40790a2f404f91f060a8
WangQY17 <wqy17nju@163.com>
2022-02-14 10:16:02 +0800

    change import test

--- src/test/java/com/agile/train/TrainApplicationTests.java
+++ src/test/java/com/agile/train/TrainApplicationTests.java
@@ -1,6 +1,7 @@
 package com.agile.train;
 
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
 //import org.testng.annotations.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
