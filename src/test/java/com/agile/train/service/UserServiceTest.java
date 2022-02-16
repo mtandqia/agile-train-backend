ab54e112a89688cadf1399706b7690fc108c2a9b
lmt019 <934808050@qq.com>
2022-02-16 22:34:38 +0800

    chore: try push

--- src/test/java/com/agile/train/service/UserServiceTest.java
+++ src/test/java/com/agile/train/service/UserServiceTest.java
@@ -49,6 +49,7 @@ class UserServiceTest {
         UserModifyDTO userModifyDTO=new UserModifyDTO(user.getId(),"abdabd",user.getEmail(),"password");
         userService.updateUser(userModifyDTO);
         userService.deleteUser("abdabd");
+        g
     }
 
     @Test
